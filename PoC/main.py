047291b2792945214cd0a0cd4679a3263bbea262
Abdullahi Farah <abdu_8524@protonmail.com>
2025-03-26 09:47:20 +0100

    Error and config

--- PoC/main.py
+++ PoC/main.py
@@ -20,19 +20,36 @@ Options:
   -h, --help  Show this help message and exit
 """
 
+"""
+#define ELAN_MOC_DRIVER_FULLNAME "Elan MOC Sensors"
+#define ELAN_M0C_CMD_LEN 0x3
+#define ELAN_EP_CMD_OUT (0x1 | LIBUSB_ENDPOINT_OUT)
+#define ELAN_EP_CMD_IN (0x3 | LIBUSB_ENDPOINT_IN)
+#define ELAN_EP_MOC_CMD_IN (0x4 | LIBUSB_ENDPOINT_IN)
+#define ELAN_EP_IMG_IN (0x2 | LIBUSB_ENDPOINT_IN)
+
+"""
+
 VENDOR_ID = 0x04F3
 PRODUCT_ID = 0x0C4C
 SUPPORTED_VERSION = 3.05
 
+CONFIG = {
+    5000 : "ELAN_CMD_TIMEOUT",
+    92: "ELAN_MAX_USER_ID_LEN",
+    9: "ELAN_MAX_ENROLL_NUM",
+    500: "ELAN_CAL_RETRY"
+}
+
 ERRORS = {
-    0x41: "",
-    0x42: "",
-    0x43: "",
-    0x44: "",
-    0xfb: "",
-    0xfd: "",
-    0xfe: "",
-    0xdd: ""
+    0xfd: "ELAN_MSG_VERIFY_ERR",
+    0xfb: "ELAN_MSG_DIRTY",
+    0xfe: "ELAN_MSG_AREA_NOT_ENOUGH",
+    0x41: "ELAN_MSG_TOO_HIGH",
+    0x42: "ELAN_MSG_TOO_LEFT",
+    0x43: "ELAN_MSG_TOO_LOW",
+    0x44: "ELAN_MSG_TOO_RIGHT",
+    0x00: "ELAN_MSG_OK"
 }
 
 def handle_info():
