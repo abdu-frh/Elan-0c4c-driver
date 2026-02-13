1a3f133a5f10fcee5ea841ecc39ba4b971370dd5
Abdullahi Farah <abdu_8524@protonmail.com>
2026-02-13 01:57:56 +0100

    seperating

--- PoC/wireshark_nb.py
+++ PoC/wireshark_nb.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.19.9"
+__generated_with = "0.19.11"
 app = marimo.App(width="medium")
 
 
@@ -110,11 +110,7 @@ def _():
 def _():
     VENDOR_ID = 0x04F3
     PRODUCT_ID = 0x0C4C
-
-    # Endpoint addresses (from your USBView dump)
-    EP_OUT = 0x01  # Endpoint 1 OUT (Commands)
-    EP_IN = 0x81   # Endpoint 1 IN (Responses)
-    return EP_IN, EP_OUT, PRODUCT_ID, VENDOR_ID
+    return PRODUCT_ID, VENDOR_ID
 
 
 @app.cell(hide_code=True)
@@ -133,8 +129,162 @@ def _(mo):
     return
 
 
+app._unparsable_cell(
+    r"""
+    import usb.core
+    import usb.util
+    import libusb_package
+
+    # Get the bundled backend
+    backend = libusb_package.get_libusb1_backend()
+
+    def find_device(backend=libusb_package, vendor_id, product_id):
+        dev = usb.core.find(backend=backend, idVendor=vendor_id, idProduct=product_id)
+        if dev is None:
+            raise ValueError("Device not found")
+    
+        return dev 
+
+    def config_device(dev):
+        dev.set_configuration()
+    
+        cfg = dev.get_active_configuration()
+        intf = cfg[(0, 0)]  # Interface 0, Alternate setting 0
+    
+        # Detach kernel driver if attached (Linux/macOS)
+        if dev.is_kernel_driver_active(0):
+            print("Detaching kernel driver...")
+            dev.detach_kernel_driver(0)
+    
+        # Claim interface
+        usb.util.claim_interface(dev, 0)
+
+        return intf
+    
+    def find_and_config_device():
+        dev = find_device()
+        intf = config_device(dev)
+        return dev intf
+
+    """,
+    name="_"
+)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ##Searching for USB Endpoints
+    """)
+    return
+
+
+app._unparsable_cell(
+    r"""
+    device intf = find_and_config_device()
+
+    def find_Endpoints(usb,intf):
+        ep_out = usb.util.find_descriptor(
+            intf,
+            custom_match=lambda e: 
+                usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_OUT
+        )
+        ep_in = usb.util.find_descriptor(
+            intf,
+            custom_match=lambda e: 
+                usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_IN
+        )
+        return ep_out ep_int
+    """,
+    name="_"
+)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Found USB Endpoints
+    """)
+    return
+
+
 @app.cell
-def _(EP_IN, EP_OUT, PID, PRODUCT_ID, VENDOR_ID, VID):
+def _():
+    EP_OUT = 0x01  
+    EP_IN  = 0x81  
+    return EP_IN, EP_OUT
+
+
+app._unparsable_cell(
+    r"""
+    from dataclasses import dataclass
+    import time
+
+    TIMEOUT_MS = 5000
+    CMD_PORT = 0x40
+
+    @dataclass
+    class elan_cmd:
+        name: str
+        cmd: hex
+        payload: hex
+        resp_len: bytes
+    
+    send_cmd(dev,cmd:elan_cmd):
+        try:
+            print(f"Sending {cmd.name} command...")
+
+            CMD = bytes([CMD_PORT, cmd.cmd])  
+
+            # WRITE PHASE: Send to EP1 OUT (0x01)
+            bytes_written = dev.write(EP_OUT, CMD, timeout=TIMEOUT_MS)
+            print(f"Sent {bytes_written} bytes")
+
+            print("Waiting for device processing...")
+            time.sleep(0.05)
+
+            # READ PHASE: Read from EP1 IN (0x81)
+            resp = dev.read(EP_IN, 64, timeout=TIMEOUT_MS)
+
+            # Parse response (first resp_len bytes are valid)
+            if len(resp) >= EXPECTED_RESP_LEN:
+                version_major = resp[0]
+                version_minor = resp[1]
+                version_combined = (resp[0] << 8) | resp[1]
+
+                print(f"Response raw: {resp[:EXPECTED_RESP_LEN].hex()}")
+                print(f"Firmware Version: {version_major}.{version_minor} (0x{version_combined:04X})")
+                return resp[:EXPECTED_RESP_LEN]
+            else:
+                print(f"Short response: {resp.hex()}")
+                return resp
+
+        except usb.core.USBTimeoutError:
+            print("ERROR: Timeout waiting for response. Is the device ready?")
+            raise
+        except Exception as e:
+            print(f"ERROR: {e}")
+            raise
+        finally:
+            # Cleanup
+            usb.util.release_interface(dev, 0)
+            print("Interface released")
+    
+    """,
+    name="_"
+)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Get Firmware Version
+    """)
+    return
+
+
+@app.cell
+def _(EP_IN, EP_OUT, PRODUCT_ID, TIMEOUT_MS, VENDOR_ID):
     import usb.core
     import usb.util
     import time
@@ -145,18 +295,17 @@ def _(EP_IN, EP_OUT, PID, PRODUCT_ID, VENDOR_ID, VID):
 
     FW_VER_CMD = bytes([0x40, 0x19])  
     EXPECTED_RESP_LEN = 2             
-    TIMEOUT_MS = 1000     
 
     def send_elan_cmd():
         # Find device
         dev = usb.core.find(backend=backend,idVendor=VENDOR_ID, idProduct=PRODUCT_ID)
         if dev is None:
-            raise ValueError(f"Device {VID:04x}:{PID:04x} not found. Check PID!")
+            raise ValueError(f"Device not found")
 
         print(f"Found device: {PRODUCT_ID}")
 
        # Unmark if device driver is not configured by os
-       #dev.set_configuration()
+        dev.set_configuration()
 
         cfg = dev.get_active_configuration()
         intf = cfg[(0, 0)]  # Interface 0, Alternate setting 0
